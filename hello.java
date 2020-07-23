37a5a85941878225afce913ab4c2de7edab605fb
Rain999 <1163799833@qq.comgit>
2020-07-23 11:12:19 +0800

    other java

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class HelloWorld(){
+	public static void main(String [] args){
+		System out.println("1");
+		System out.println("4");
+		System out.println("5");
+		System out.println("6");
+		System out.println("7");
+		
+	}
+}
